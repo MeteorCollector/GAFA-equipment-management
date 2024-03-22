39251db1fa4657fee39c61626a9ca109e9c2af74
MeteorCollector <shaoyuqian2010@163.com>
2024-03-22 17:09:13 +0800

    added saving function

--- python_back/main.py
+++ python_back/main.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 from uuid import uuid4
 from flask_cors import CORS
 import os
+import json
 
 app = Flask(__name__)
 CORS(app)
+
+# 定义存储数据的文件路径
+DATA_FILE = 'materials.json'
 
 FRONT_ROOT = '../web_front/'
 UPLOAD_FOLDER = 'uploads'  # 设置上传文件存储目录
@@ -12,6 +16,15 @@ if not os.path.exists(FRONT_ROOT + UPLOAD_FOLDER):
     os.makedirs(FRONT_ROOT + UPLOAD_FOLDER)
 
 materials = []
+
+# 如果数据文件不存在，则创建一个空列表
+if not os.path.exists(DATA_FILE):
+    with open(DATA_FILE, 'w') as f:
+        json.dump([], f)
+
+# 读取数据文件中的数据到内存中
+with open(DATA_FILE, 'r') as f:
+    materials = json.load(f)
 
 # 获取所有物资
 @app.route('/materials', methods=['GET'])
@@ -38,6 +51,10 @@ def add_material():
 
     new_material = {'id': id, 'name': name, 'photo': filename, 'description': description, 'location': location}
     materials.append(new_material)
+    # 更新数据文件
+    with open(DATA_FILE, 'w') as f:
+        json.dump(materials, f)
+    
     return jsonify(new_material), 201
 
 # 删除物资
@@ -45,6 +62,9 @@ def add_material():
 def delete_material(id):
     global materials
     materials = [material for material in materials if material['id'] != id]
+    # 更新数据文件
+    with open(DATA_FILE, 'w') as f:
+        json.dump(materials, f)
     return '', 204
 
 # 更新物资信息
@@ -66,6 +86,10 @@ def update_material(id):
                 if (os.path.exists(os.path.join(FRONT_ROOT, material['photo']))):
                     os.remove(os.path.join(FRONT_ROOT, material['photo']))
                 material['photo'] = filename
+
+            # 更新数据文件
+            with open(DATA_FILE, 'w') as f:
+                json.dump(materials, f)
             return jsonify(material), 201
 
     return jsonify({'error': 'Material not found'}), 404  
